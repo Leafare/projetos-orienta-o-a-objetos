0f21baa893e589ac1ea0e779797ce02c9b21716a
Rafael Magalhães Merenciano <98890621+Leafare@users.noreply.github.com>
2023-09-21 15:07:35 -0300

    Update Projeto 1.py

--- Projeto 1.py
+++ Projeto 1.py
@@ -1,22 +1,19 @@
 
-#  Projeto 1 de Orientacao a objetos
+# Projeto 1 de Orientacao a objetos
 # Pseudocodigo
 
 # Inicio
 # Criar a classe quadrado
 # definir os atributos da classe quadrado
-class quadrado():
-  def __init__(self,lado):
-      self.lado = lado
-
-
+class Quadrado():
+    def __init__(self,lado):
+        self.lado = lado
 # definir os metodos da classe quadrado
-
-def calcularArea(self):
-  return ( self.lado**2)
+    def calcularArea(self):
+        return self.lado**2
 
 # Criar uma instancia da classe quadrado com um lado de 5
-quadrado = quadrado(5)
+quadrado = Quadrado(5)
 
 # Calcular a area do quadrado
 area = quadrado.calcularArea()
@@ -30,19 +27,19 @@ print("Área do quadrado:", area)
 
 
 # Criar a classe retangulo
-class retangulo():
+class Retangulo():
 #definir os atributos da classe retangulo
 
 # definir os metodos da classe retangulo
- def __init__(self, comprimento, largura):
-  self.comprimento = comprimento
-  self.largura = largura
+    def __init__(self, comprimento, largura):
+        self.comprimento = comprimento
+        self.largura = largura
 
 
-def cacularArea(self):
- self.area = ( self.comprimento*self.largura)
+    def calcularArea(self):
+        return self.comprimento*self.largura
 
-retangulo = retangulo(4, 6)
+retangulo = Retangulo(4, 6)
 
 for i in range(retangulo.largura):
     for j in range(retangulo.comprimento):
@@ -53,19 +50,19 @@ for i in range(retangulo.largura):
 print("Área do retângulo:", area)
 # Criar a classe ponto
 
-class ponto():
+class Ponto():
   # definir os atributos da classe ponto como uma coordenada (x,y) e raio como um numero positivo
-  def __init__(self,x,y):
-   self.x = x
-   self.y = y
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
 
 # definir os metodos da classe ponto
-  def mover(self, delta_x, delta_y):
-    self.x += delta_x
-    self.y += delta_y 
+    def mover(self, delta_x, delta_y):
+        self.x += delta_x
+        self.y += delta_y 
 
 # Criar uma instância de Ponto
-ponto = ponto(3, 5)  # Por exemplo, um ponto com coordenadas (3, 5)
+ponto = Ponto(3, 5)  # Por exemplo, um ponto com coordenadas (3, 5)
 
 # Imprimir a figura do ponto
 print("Coordenadas do ponto:", ponto.x, ponto.y)
@@ -74,17 +71,13 @@ print("Coordenadas do ponto:", ponto.x, ponto.y)
 # Criar a classe circulo
 #utilizar o conceito de heranca e herdar os atributos e metodos da classe ponto
 
-class circulo(ponto):
-  def __init__(self, x, y, raio):
+class Circulo(Ponto):
+    def __init__(self, x, y, raio):
         super().__init__(x, y)  
         self.raio = raio
 
-  def mover(self, delta_x, delta_y):
-    self.x += delta_x
-    self.y += delta_y 
-
 # Criar uma instancia de Circulo
-circulo = circulo(3, 5, 2)  # Por exemplo, um circulo com centro (3, 5) e raio 2
+circulo = Circulo(3, 5, 2)  # Por exemplo, um circulo com centro (3, 5) e raio 2
 
 # Imprimir a figura do circulo
 print("Centro do circulo (coordenadas x, y):", circulo.x, circulo.y)
